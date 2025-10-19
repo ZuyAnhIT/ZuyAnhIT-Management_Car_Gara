1113d43f902cdf8baff85cb9388289a3ae7e846d
ZiyAnkIT <anhtrauluoi@gmail.com>
2025-10-19 09:35:03 +0700

    Them repository ChiTietPhieuSuaChua HoaDon PhieuSuaChua

---
+++ Backend_Gara/gara-management/src/main/java/com/example/gara_management/repository/PhieuSuaChuaRepository.java
@@ -0,0 +1,25 @@
+package com.example.gara_management.repository;
+
+import com.example.gara_management.model.PhieuSuaChua;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PhieuSuaChuaRepository extends JpaRepository<PhieuSuaChua, Integer>, JpaSpecificationExecutor<PhieuSuaChua> {
+    
+    List<PhieuSuaChua> findByTrangThai(String trangThai);
+    
+    List<PhieuSuaChua> findByXe_MaXe(Integer maXe);
+    
+    List<PhieuSuaChua> findByTho_MaTho(Integer maTho);
+    
+    @Query("SELECT p FROM PhieuSuaChua p " +
+           "LEFT JOIN FETCH p.chiTietList ct " +
+           "LEFT JOIN FETCH ct.dichVu " +
+           "WHERE p.maPhieu = :maPhieu")
+    PhieuSuaChua findByIdWithDetails(Integer maPhieu);
+}
+
