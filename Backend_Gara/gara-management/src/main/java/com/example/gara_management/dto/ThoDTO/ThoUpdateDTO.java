37532b4f4060ae47374cbf63c4ca61514409d10e
NguyenDongQuan <qwanng06@gmail.com>
2025-10-24 15:04:16 +0700

    Tho Update Validation

--- Backend_Gara/gara-management/src/main/java/com/example/gara_management/dto/ThoDTO/ThoUpdateDTO.java
+++ Backend_Gara/gara-management/src/main/java/com/example/gara_management/dto/ThoDTO/ThoUpdateDTO.java
@@ -1,15 +1,35 @@
 package com.example.gara_management.dto.ThoDTO;
 
+
 import lombok.*;
+import jakarta.validation.constraints.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ThoUpdateDTO {
+
+    @NotBlank(message =  "Tên Thợ không được để trống" )
     private String tenTho;
+
+    @NotBlank(message =  "Chuyên môn kkhông được để trống" )
     private String chuyenMon;
+
+    @NotBlank(message =  "Số điện thoại không được để trống" )
+    @Pattern(
+        regexp = "^(0[0-9]{9}|\\+84\\s?[0-9]{9})$",
+        message = "Số điện thoại VN không hợp lệ (vd: 0901234567, +84901234567, hoặc +84 901234567)"
+    )
     private String soDienThoai;
+
+    @NotBlank(message =  "Email không được để trống" )
+    @Email(message =  "Email không hợp lệ" )
     private String email;
+
+    @Min(value = 0, message = "Kinh nghiệm không được âm")
+    @Max(value = 50, message = "Kinh nghiệm không được vượt quá 50 năm")
     private Integer kinhNghiem;
+
+
     private String trangThai;
 }
